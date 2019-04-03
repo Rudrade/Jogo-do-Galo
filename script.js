eb75fd50ed2eccb3be2cff155dae34a93c4c78d2
Rui Andrade <andrade.rui99@gmail.com>
2019-04-03 16:18:54 +0100

    Adição de jogar contra computador

--- script.js
+++ script.js
@@ -4,40 +4,57 @@ var tabuleiro = [
   [document.getElementById("20"), document.getElementById("21"), document.getElementById("22")]
 ];
 var jogador = 1;
-var imagemX = document.getElementById("x").src;
-var imagemO = document.getElementById("o").src;
-var transparente = document.getElementById("00").src;
-var estadoJogo = 1;
+const imagemX = document.getElementById("x").src;
+const imagemO = document.getElementById("o").src;
+const transparente = document.getElementById("00").src;
+var estadoJogo = true;
 var pontuacaoJ1 = 0, pontuacaoJ2 = 0, pontuacaoE = 0;
 var jogadas = 0;
-var computador = 0;
+var computador = false;
 
-//267
-function validacao(jogador) {
-  if (tabuleiro[0][0].src == tabuleiro[1][1].src && tabuleiro[1][1].src == tabuleiro[2][2].src && tabuleiro[2][2].src != transparente) {
-    estadoJogo = 0;
+function validacao(tabuleiroVal, bdVal) {
+  if (tabuleiroVal[0][0] == tabuleiroVal[1][1] && tabuleiroVal[1][1] == tabuleiroVal[2][2] && tabuleiroVal[2][2] != transparente) {
+    console.log("1");
+    if (bdVal) {
+      return true;
+    }
+    estadoJogo = false;
   }
-  else if (tabuleiro[0][2].src == tabuleiro[1][1].src && tabuleiro[1][1].src == tabuleiro[2][0].src && tabuleiro[0][2].src != transparente) {
-    estadoJogo = 0;
+  else if (tabuleiroVal[0][2] == tabuleiroVal[1][1] && tabuleiroVal[1][1] == tabuleiroVal[2][0] && tabuleiroVal[0][2] != transparente) {
+console.log("2");
+    if (bdVal) {
+      return true;
+    }
+    estadoJogo = false;
   }
   else {
     for (var i = 0; i < 3; i++) {
-      if (tabuleiro[i][0].src == tabuleiro[i][1].src && tabuleiro[i][1].src == tabuleiro[i][2].src && tabuleiro[i][2].src != transparente) {
-        estadoJogo = 0;
+      if (tabuleiroVal[i][0] == tabuleiroVal[i][1] && tabuleiroVal[i][1] == tabuleiroVal[i][2] && tabuleiroVal[i][2] != transparente) {
+        console.log("3");
+        if (bdVal) {
+          return true;
+        }
+        estadoJogo = false;
         break;
       }
-      else if (tabuleiro[0][i].src == tabuleiro[1][i].src && tabuleiro[0][i].src == tabuleiro[2][i].src && tabuleiro[0][i].src != transparente) {
-        estadoJogo = 0;
+      else if (tabuleiroVal[0][i] == tabuleiroVal[1][i] && tabuleiroVal[0][i] == tabuleiroVal[2][i] && tabuleiroVal[0][i] != transparente) {
+        console.log("4");
+        if (bdVal) {
+          return true;
+        }
+        estadoJogo = false;
         break;
       }
     }
   }
-
-  if (estadoJogo == 1) {
-    jogadas++;
+  if (bdVal) {
+    return false;
+  }
+  if (estadoJogo) {
+    ++jogadas;
     if (jogadas == 9) {
       document.getElementById("totalE").innerHTML = ++pontuacaoE;
-      estadoJogo = 0;
+      estadoJogo = false;
     }
   }
   else {
@@ -50,22 +67,94 @@ function validacao(jogador) {
   }
 }
 
+function copiarTabuleiros() {
+  var tabuleiroAI = [
+    [tabuleiro[0][0].src, tabuleiro[0][1].src, tabuleiro[0][2].src],
+    [tabuleiro[1][0].src, tabuleiro[1][1].src, tabuleiro[1][2].src],
+    [tabuleiro[2][0].src, tabuleiro[2][1].src, tabuleiro[2][2].src]
+  ];
+  return tabuleiroAI;
+}
+
+function computadorAI() {
+  switch (jogadas) {
+    case 1:
+        if (tabuleiro[1][1].src != imagemX) {
+          tabuleiro[1][1].src = imagemO;
+          jogador = 1;
+          ++jogadas;
+        }
+        else {
+
+        }
+      break;
+    case 3:
+        var  a = false;
+        for (var i = 0; i < 3; i++) {
+          for (var y = 0; y < 3; y++) {
+            var tabuleiroAI = copiarTabuleiros();
+            if (tabuleiroAI[i][y] == transparente) {
+              if (!a) {
+                tabuleiroAI[i][y] = imagemX;
+                if (validacao(tabuleiroAI, true)) {
+                  tabuleiro[i][y].src = imagemO;
+                  jogador = 1;
+                  ++jogadas;
+                  return;
+                }
+                if (i == 2 && y == 2) {
+                  i = 0;
+                  y = 0;
+                  a = true;
+                }
+              }
+              else {
+                console.log(copiarTabuleiros());
+                var tabuleiroAI2 = copiarTabuleiros();
+                console.log(tabuleiroAI2);
+                tabuleiroAI2[i][y] = imagemO;
+                for (var q = 0; q < 3; q++)  {
+                  for (var w = 0; w < 3; w++) {
+                    if (tabuleiroAI2[q][w] == transparente) {
+                      tabuleiroAI2[q][w] = imagemO;
+                      if (validacao(tabuleiroAI2, true)) {
+                        tabuleiro[i][y].src = imagemO;
+                        jogador = 1;
+                        ++jogadas;
+                        return;
+                      }
+                    }
+                    console.log("q: " + q);
+                    console.log("w: " + w);
+                    if (q == 2 && w == 2) {
+                      console.log("a");
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      break;
+  }
+}
+
 function mudarImagem(quadrado) {
-  if (estadoJogo == 1) {
+  if (estadoJogo) {
     if (jogador == 1 && quadrado.src == transparente) {
       quadrado.src = imagemX;
-      validacao(jogador);
-      if (computador == 0) {
+      validacao(copiarTabuleiros(), false);
+      if (!computador) {
         jogador = 2;
       }
       else {
-        computador(quadrado.id);
+        computadorAI();
       }
       document.getElementById("turnoI").src = imagemO;
     }
     else if (jogador == 2 && quadrado.src == transparente) {
       quadrado.src = imagemO;
-      validacao(jogador);
+      validacao(copiarTabuleiros(), false);
       jogador = 1;
       document.getElementById("turnoI").src = imagemX;
     }
@@ -75,29 +164,12 @@ function mudarImagem(quadrado) {
 function recomecar() {
   jogador = 1;
   jogadas = 0;
-  estadoJogo = 1;
+  estadoJogo = true;
 
   for (var i = 0; i < 3; i++) {
     for (var y = 0; y < 3; y++) {
       tabuleiro[i][y].src = transparente;
     }
-  }
-}
-
-function computador(quadrado) {
-  switch (jogadas) {
-    case 0:
-      tabuleiro[0][0].src = imagemO;
-    break;
-    case 2:
-      for (var i = 0; i < 3; i++) {
-        for (var y = 0; y < 3; y++) {
-          if (tabuleiro[i][y].src = imagemX) {
-            jogadaInicial = tabuleiro[i][y].id;
-          }
-        }
-      }
-    break;
   }
 }
 
@@ -109,6 +181,14 @@ window.onload = function() {
       });
     }
   }
+
+  document.getElementById("computador").addEventListener("click", function() {
+    recomecar();
+    jogador = 1;
+    computador = true;
+    estadoJogo = true;
+    jogadas = 0;
+  });
 
   document.getElementById("reset").addEventListener("click", function () {
     recomecar();
